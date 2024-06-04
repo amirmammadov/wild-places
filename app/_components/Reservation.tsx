3952b90bf2e2735dee5030bb2f6ae15b828400de
amirmammadov <amirmammado@gmail.com>
2024-06-04 16:10:51 +0400

    reservation calendar and form was added with suspense

---
+++ app/_components/Reservation.tsx
@@ -0,0 +1,24 @@
+import DateSelector from "@/app/_components/DateSelector";
+import ReservationForm from "@/app/_components/ReservationForm";
+
+import { getSettings, getBookedDatesByCabinId } from "@/app/_lib/data-service";
+
+const Reservation = async ({ cabin }: { cabin: any }) => {
+  const [settings, bookedDates] = await Promise.all([
+    getSettings(),
+    getBookedDatesByCabinId(cabin.id),
+  ]);
+
+  return (
+    <div className="grid grid-cols-2 border border-primary-800 min-h-[400px]">
+      <DateSelector
+        settings={settings}
+        bookedDates={bookedDates}
+        cabin={cabin}
+      />
+      <ReservationForm cabin={cabin} />
+    </div>
+  );
+};
+
+export default Reservation;
--- app/_components/SelectCountry.tsx
+++ app/_components/SelectCountry.tsx
@@ -1,7 +1,5 @@
 import { getCountries } from "@/app/_lib/data-service";
 
-// Let's imagine your colleague already built this component 😃
-
 async function SelectCountry({
   defaultCountry,
   name,
@@ -22,7 +20,6 @@ async function SelectCountry({
     <select
       name={name}
       id={id}
-      // Here we use a trick to encode BOTH the country name and the flag into the value. Then we split them up again later in the server action
       defaultValue={`${defaultCountry}%${flag}`}
       className={className}
     >
