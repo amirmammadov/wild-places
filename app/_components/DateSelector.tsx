6353bb5b44fb52a9d9eef9d4563cdcd5c3ee15fe
amirmammadov <amirmammado@gmail.com>
2024-06-16 16:34:16 +0400

    date selector was finished

--- app/_components/DateSelector.tsx
+++ app/_components/DateSelector.tsx
@@ -4,7 +4,12 @@ import { SetStateAction } from "react";
 
 import { useReservation, ReservationState } from "./ReservationContext";
 
-import { isWithinInterval } from "date-fns";
+import {
+  differenceInDays,
+  isPast,
+  isSameDay,
+  isWithinInterval,
+} from "date-fns";
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
 
@@ -29,10 +34,12 @@ function DateSelector({
 }) {
   const { range, setRange, resetRange } = useReservation();
 
-  const regularPrice = 23;
-  const discount = 23;
-  const numNights = 23;
-  const cabinPrice = 23;
+  const { regularPrice, discount } = cabin;
+  const numNights =
+    range && range.from && range.to
+      ? differenceInDays(range?.to, range?.from)
+      : 0;
+  const cabinPrice = numNights * (regularPrice - discount);
 
   const { minBookingLength, maxBookingLength } = settings;
 
@@ -52,6 +59,10 @@ function DateSelector({
         toYear={new Date().getFullYear() + 5}
         captionLayout="dropdown"
         numberOfMonths={2}
+        disabled={(curDate) =>
+          isPast(curDate) ||
+          bookedDates.some((date: any) => isSameDay(date, curDate))
+        }
       />
 
       <div className="flex items-center justify-between px-8 bg-accent-500 text-primary-800 h-[72px]">
